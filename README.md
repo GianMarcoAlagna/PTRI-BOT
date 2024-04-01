a09665964347f82dd531a46953eae7ac6aefb144
GianMarcoAlagna <marcoalagna1@gmail.com>
2024-04-01 02:15:19 -0500

    Added todo to readme for now

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-A Discord Bot Developed for the unofficial Codesmith Discord, by and for the PTRI Community.
+## A Discord Bot Developed for the unofficial Codesmith Discord, by and for the PTRI Community.
+
+## Todo List
+
+Priority:
+HIGH: - [ ] Have the bot give roles based on reactions to a message - [ ] Create a command to ban a user - [ ] Create a command to kick a user
+MEDIUM: - [ ] Create a command to create a poll using reactions - [ ] Create a command to mute a user - [ ] Create a command to unmute a user
+LOW: - [ ] Allow the bot to join a voice channel - [ ] Allow the bot to play music in a voice channel
--- src/__init__/initCommands.js
+++ src/__init__/initCommands.js
@@ -6,6 +6,7 @@ import "dotenv/config";
 
 const TOKEN = process.env.TOKEN || null;
 const CLIENT_ID = process.env.CLIENT_ID || null;
+const rest = new REST().setToken(TOKEN);
 
 const __dirname = path.resolve();
 const foldersPath = path.join(__dirname, "src", "commands");
@@ -13,6 +14,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 const commands = [];
 
 async function loadCommands(client) {
+  colorize.green("Loading commands");
   if (!client) {
     throw new Error(
       "Command loader requires a client instance, received" +
@@ -20,48 +22,50 @@ async function loadCommands(client) {
         client
     );
   }
+  try {
+    colorize.green(`Found ${commandFolders.length} command folders`);
 
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs
-      .readdirSync(commandsPath)
-      .filter((file) => file.endsWith(".js"));
-    colorize.green(`Loading commands from ${folder} folder`);
+    for (const folder of commandFolders) {
+      colorize.green(`Loading commands from ${folder} folder`);
 
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-      colorize.yellow(`Processing command file ${file}`);
+      const commandsPath = path.join(foldersPath, folder);
+      const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+      for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        colorize.yellow(`Processing command file ${file}`);
 
-      //* We have to convert path to URL because ES modules use URL file paths to import modules
-      const modulePath = new URL("file:///" + filePath);
-      let command = await import(modulePath);
-      // get default export if it exists
-      command = command.default || command;
+        //* We have to convert path to URL because ES modules use URL file paths to import modules
+        const modulePath = new URL("file://" + filePath);
+        let command = await import(modulePath);
+        //* get default export if it exists
+        command = command.default || command;
 
-      // Set a new item in the Collection with the key as the command name and the value as the exported module
-      if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command);
-        commands.push(command.data.toJSON());
-        colorize.blue(`Loaded command ${command.data.name}`);
-      } else {
-        colorize.red(
-          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-        );
+        //* Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ("data" in command && "execute" in command) {
+          client.commands.set(command.data.name, command);
+          commands.push(command.data.toJSON());
+          colorize.blue(`Loaded command ${command.data.name}`);
+        } else {
+          colorize.red(
+            `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+          );
+        }
       }
     }
+  } catch (error) {
+    throw new Error("An error occurred while loading commands" + error);
   }
 }
 
-const rest = new REST().setToken(TOKEN);
-
-// and deploy your commands!
 async function deploy() {
   try {
     colorize.white(
       `Started refreshing ${commands.length} application (/) commands.`
     );
 
-    // The put method is used to fully refresh all commands in the guild with the current set
+    //* The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(Routes.applicationCommands(CLIENT_ID), {
       body: commands,
     });
@@ -70,8 +74,8 @@ async function deploy() {
       `Successfully reloaded ${data.length} application (/) commands.`
     );
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
-    console.error(error);
+    //* And of course, make sure you catch and log any errors!
+    throw new Error(error);
   }
 }
 
